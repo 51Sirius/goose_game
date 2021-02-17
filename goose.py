f6bf0a2453b7a46a3c1c2010bb16dc0a24d1663a
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-17 16:53:17 +0300

    add display settings

--- goose.py
+++ goose.py
@@ -5,5 +5,11 @@ first_color = (248, 243, 212)
 second_color = (0, 184, 169)
 third_color = (246, 65, 108)
 four_color = (255, 222, 125)
+display_size = (1000, 600)
+display = pg.display.set_mode(display_size)
+pg.init()
+pg.display.set_caption('Goose Game')
 
 
+def start_game():
+    pass
