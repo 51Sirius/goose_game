fab5517dce2608b924869b9290db575c3ece02c9
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-23 02:03:35 +0300

    add main color and show menu

--- goose.py
+++ goose.py
@@ -1,6 +1,5 @@
 import pygame as pg
 
-
 first_color = (248, 243, 212)
 second_color = (0, 184, 169)
 third_color = (246, 65, 108)
@@ -8,8 +7,19 @@ four_color = (255, 222, 125)
 display_size = (1000, 600)
 display = pg.display.set_mode(display_size)
 pg.init()
+clock = pg.time.Clock()
 pg.display.set_caption('Goose Game')
 
 
 def start_game():
-    pass
+    show_menu = True
+    while show_menu:
+        event = pg.event.poll()
+        if event.type == pg.QUIT:
+            exit()
+        display.fill(first_color)
+        pg.display.update()
+        clock.tick(60)
+
+
+start_game()
